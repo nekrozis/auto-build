5bcd6560ac718d5bfe839d3faa1d3c15e64daa7b
nekrozis <mgal25@outlook.com>
2025-08-29 23:32:47 +0800

    update

--- script/nu.sh
+++ script/nu.sh
@@ -1,9 +1,12 @@
 git clone https://github.com/nushell/nushell.git && cd nushell
 git checkout -b $COMMIT
 
-export RUSTFLAGS="-C opt-level=3 -C target-cpu=x86-64-v3"
-export _CL_="/arch:AVX2 /O2 /GL /fp:fast"
-export _LINK_="/LTCG /OPT:REF /OPT:ICF"
+# export RUSTFLAGS="-C opt-level=3 -C target-cpu=x86-64-v3"
+# export _CL_="/arch:AVX2 /O2 /GL /fp:fast"
+# export _LINK_="/LTCG /OPT:REF /OPT:ICF"
+export RUSTFLAGS=" -C target-cpu=x86-64-v3"
+export _CL_=" /arch:AVX2"
+
 
 cargo fetch --locked --target x86_64-pc-windows-msvc
 cargo build --release --frozen --workspace
